71214188334734b34b2d0bc2e984b1887f82447d
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-06-03 22:43:04 +0900

    Create programmers_middle.java
    알고리즘

---
+++ programmers_middle.java
@@ -0,0 +1,16 @@
+package solution;
+
+import java.util.Scanner;
+
+class programmers_middle {
+    public String solution(String s) {
+       
+        String answer = "";
+         if(s.length()%2==1){
+            answer=s.substring(s.length()/2,s.length()/2+1);
+        }else{
+            answer=s.substring(s.length()/2-1,s.length()/2+1);
+        }
+        return answer;
+    }
+}
