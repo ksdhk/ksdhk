9fd3f9395f8002d73142fe6c08c2754d1f62d50d
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-06-15 23:59:01 +0900

    Create programmers_kim.java

---
+++ programmers_kim.java
@@ -0,0 +1,17 @@
+package solution;
+
+import java.util.*;
+
+class programmers_kim {
+
+	public String solution(String[] seoul) {
+        String answer = "";
+      for(int i=0;i<seoul.length;i++){
+        if(seoul[i].equals("Kim")){
+            answer="김서방은 "+i+"에 있다";
+        };
+      }
+    
+      return answer;
+  }
+}
