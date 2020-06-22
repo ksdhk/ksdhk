4b49a95a7d1aa744acb45feefffc747c9d6f3325
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-06-23 02:27:21 +0900

    Create programmers_avg.java

---
+++ programmers_avg.java
@@ -0,0 +1,20 @@
+package solution;
+
+import java.util.*;
+
+class programmers_avg {
+
+	class Solution {
+	    public double solution(int[] arr) {
+	        double answer = 0;
+	        int sum = 0;
+	        for(int i=0;i<arr.length;i++){
+	            
+	            sum+=arr[i];
+	        }
+	       answer = (double)sum/arr.length;
+	        
+	        return answer;
+	    }
+	}
+}
