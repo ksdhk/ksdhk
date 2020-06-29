7588074f661c0f39931d00984b47ece65bf7d1ba
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-06-30 00:21:21 +0900

    Create programmers_odd.java

---
+++ programmers_odd.java
@@ -0,0 +1,17 @@
+package solution;
+
+import java.util.*;
+
+class programmers_odd {
+
+    public String solution(int num) {
+        String answer ="";
+       if(num%2==0){
+        answer = "Even";
+       }
+       else{
+             answer = "Odd";
+       }
+       return answer;
+   }
+}
