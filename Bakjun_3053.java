5c97857fd2324b5b997db5cf28a3c3f49a80f569
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-07-03 00:12:49 +0900

    Create Bakjun_3053.java

---
+++ Bakjun_3053.java
@@ -0,0 +1,20 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Bakjun_3053 {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int A = sc.nextInt();
+	
+	    double result = A*A*3.1415926535897932384626433;
+	    double result2= A*A*2;
+	    
+	    System.out.println(result);
+	    System.out.println(result2);
+	    sc.close();
+		
+	 
+	    
+	}
+}
