d351083b155afb7c17182c5e9e01a8580a33cb5c
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-08-15 01:16:55 +0900

    백준 1427/ 2020-08-15

---
+++ solution/Bakjun_1427.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bakjun_1427{
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		String N = sc.nextLine();
+	
+		char[] a= new char[N.length()];
+	   
+		for(int i=0;i<a.length;i++) {
+			a[i]=N.charAt(i);
+			}
+		
+		Arrays.sort(a);
+		
+		for(int i=a.length-1;i>-1;i--) {
+		System.out.print(a[i]);
+		}
+		sc.close();
+	}
+}
