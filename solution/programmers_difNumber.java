c4a26e95a9c4ce4826733edb5736b4281aa27c27
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-12-05 23:44:11 +0900

    diff_number

--- solution/programmers_difNumber.java
+++ solution/programmers_difNumber.java
@@ -3,13 +3,18 @@ package solution;
 import java.util.*;
 
 class programmers_difNumber {
-
-    public int[] solution(int []arr) {
-        int[] answer = {};
-        
-        answer[1] = 0;
-        System.out.println("Hello Java");
-
-        return answer;
-    }
+	  public int[] solution(int []arr) {
+	        ArrayList<Integer> list = new ArrayList();
+	        for(int i=0;i<arr.length;i++){
+	         if(i==0||i>0&&arr[i-1]!=arr[i]){    
+	         list.add(arr[i]) ;
+	         }
+	        }
+	        int[] answer = new int[list.size()];
+	        for(int j=0;j<list.size();j++){
+	            answer[j] = list.get(j);
+	        }
+	      
+	        return answer;
+	    }
 }
