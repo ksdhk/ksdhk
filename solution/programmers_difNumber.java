7f86771c3eb2535c337e34193e58f3d7cb91d5e0
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-12-05 23:20:20 +0900

    test

---
+++ solution/programmers_difNumber.java
@@ -0,0 +1,15 @@
+package solution;
+
+import java.util.*;
+
+class programmers_difNumber {
+
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        answer[1] = 0;
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
